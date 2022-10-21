8bf9dada1b4ee1dea937b05baaf73ed340b6f9a3
Andrew Ford <andrew@andrewford.co.nz>
2022-10-21 18:21:23 +1300

    switched var to const

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 const usersRepository = require("../repository/users");
 
 /* GET users listing. */
